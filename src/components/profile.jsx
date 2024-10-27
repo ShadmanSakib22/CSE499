f501b780dc4ed4de09edf387c71d1342a230ac1e
shadmansakib22 <shadman.sakib11@northsouth.edu>
2024-10-27 11:27:01 +0600

    display rating on profile

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -19,6 +19,8 @@ function UserProfile() {
   const [verification, setVerification] = useState("");
   const [subscriptionStatus, setSubscriptionStatus] = useState("");
   const [onlineStatus, setOnlineStatus] = useState("");
+  const [rating, setRating] = useState(0);
+  const [totalRatings, setTotalRatings] = useState(0);
   const [profilePicture, setProfilePicture] = useState(null);
   const [previewImage, setPreviewImage] = useState(null);
   const [password, setPassword] = useState("");
@@ -50,6 +52,8 @@ function UserProfile() {
         setVerification(data.verification || "");
         setSubscriptionStatus(data.subscription_status || "");
         setOnlineStatus(data.login_status || "");
+        setRating(data.rating || 0);
+        setTotalRatings(data.totalRatings || 0);
       }
     } catch (error) {
       console.error("Error fetching user data:", error);
@@ -148,6 +152,28 @@ function UserProfile() {
             <p className="text-md mb-1">{subscriptionStatus}</p>
             <p className="text-md bg-gray-100 font-semibold">User is:</p>
             <p className="text-md mb-1">{onlineStatus}</p>
+            <p className="text-md bg-gray-100 font-semibold">Rating:</p>
+            <div className="pt-2">
+              <div className="flex">
+                {[1, 2, 3, 4, 5].map((star) => (
+                  <span key={star}>
+                    <svg
+                      className={`w-6 h-6 ${
+                        star <= rating ? "text-yellow-400" : "text-gray-300"
+                      }`}
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                    </svg>
+                  </span>
+                ))}
+              </div>
+              <div className="text-gray-600">
+                ({rating?.toFixed(1) || 0} / 5) from {totalRatings || 0} ratings
+              </div>
+            </div>
           </div>
 
           <hr className="clearfix w-100" />
